7a4e5e9484dbe302ef7643a81a4ba0f3a53a00ec
Ryan Wendling <57023233+Ryanwendling17@users.noreply.github.com>
2020-11-25 11:59:44 -0600

    Delete gtag.js

--- assets/gtag.js
+++
@@ -1,8 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-RX0YGPMQKW"></script>
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-RX0YGPMQKW');
