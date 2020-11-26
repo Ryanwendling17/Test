b630a933dedcbc25f80c23f24958d268af59faee
Ryan Wendling <57023233+Ryanwendling17@users.noreply.github.com>
2020-11-25 22:58:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -372,7 +372,7 @@ layout_TS = go.Layout(
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],)
 server = app.server
 
 
